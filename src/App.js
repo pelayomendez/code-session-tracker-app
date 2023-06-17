ff1f3f6c706d08b9e507c6e690d582b5ee38c69e
Pelayo Méndez <1971236+pelayomendez@users.noreply.github.com>
2023-06-17 13:26:38 +0200

    feat: Added logo

--- src/App.js
+++ src/App.js
@@ -12,8 +12,25 @@ import DescriptionResume from './DescriptionResume';
 const App = () => {
   return (
     <DatabaseProvider>
-      <div>
-        <h1>Code Session Tracker</h1>
+      <div className="app">
+      <div className="logo-container">
+           <svg
+            className="logo-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="#a8a8a8"
+            stroke="#a8a8a8"
+            strokeWidth="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M4 8h16M4 16h16" />
+            <circle cx="12" cy="12" r="1" />
+            <circle cx="12" cy="4" r="1" />
+            <circle cx="12" cy="20" r="1" />
+          </svg>
+          <h1 className="logo-text">Code Session Tracker</h1>
+        </div>
         <Tabs defaultActiveKey="1">
           <Tabs.TabPane tab="Time Tracker" key="1">
             <TimeTracker />
